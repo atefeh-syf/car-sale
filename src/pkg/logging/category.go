c990d5775fb019010cd485196669bcca56ec0239
atefeh-syf <atefe.s.77218@gmail.com>
2023-10-20 23:29:13 +0330

    logger interface

---
+++ src/pkg/logging/category.go
@@ -0,0 +1,51 @@
+package logging
+
+type Category string
+type SubCategory string
+type ExtraKey string
+
+const (
+	General 					 Category = "General"
+	Internal					  Category  = "Internal"
+	Postgres 					Category = "Postgres"
+	Redis							Category = "Redis"
+	Validation 					Category = "Validation"
+	RequestResponse	  Category = "RequestResponse"
+)
+const (
+	// General
+	StartUp SubCategory = "StartUp"
+	ExternalService SubCategory = "ExternalService"
+
+	// Postgres
+	Select SubCategory = "Select"
+	Rollback SubCategory = "Rollback"
+	Update SubCategory = "Update"
+	Delete SubCategory = "Delete"
+	Insert SubCategory = "Insert"
+
+	// Internal
+	Api SubCategory = "Api"
+	HashPassword SubCategory = "HashPassword"
+	DefaultRoleNotFound SubCategory = "DefaultRoleNotFound"
+
+
+	// Validation
+	MobileValidation SubCategory = "MobileValidation"
+	PasswordValidation SubCategory = "PasswordValidation"
+)
+
+
+const (
+	AppName ExtraKey = "AppName"
+	LoggerName ExtraKey = "LoggerName"
+	ClientIp ExtraKey = "ClientIp"
+	HostIp ExtraKey = "HostIp"
+	Method ExtraKey = "Method"
+	StatusCode ExtraKey = "StatusCode"
+	BodySize ExtraKey = "BodySize"
+	Path ExtraKey = "Path"
+	Latency ExtraKey = "Latency"
+	Body ExtraKey = "Body"
+	ErrorMessage ExtraKey = "ErrorMessage"
+)
