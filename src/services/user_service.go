44b775f4a2163bf557f5c1fca7710bfdb92a1221
atefeh-syf <atefe.s.77218@gmail.com>
2023-11-10 00:55:38 +0330

    user service changed

--- src/services/user_service.go
+++ src/services/user_service.go
@@ -4,7 +4,9 @@ import (
 	"github.com/atefeh-syf/car-sale/api/dto"
 	"github.com/atefeh-syf/car-sale/common"
 	"github.com/atefeh-syf/car-sale/config"
+	"github.com/atefeh-syf/car-sale/constants"
 	"github.com/atefeh-syf/car-sale/data/db"
+	"github.com/atefeh-syf/car-sale/data/models"
 	"github.com/atefeh-syf/car-sale/pkg/logging"
 	"gorm.io/gorm"
 )
@@ -37,3 +39,40 @@ func (s *UserService) SendOtp(req *dto.GetOtpRequest) error {
 	return nil
 }
 
+func (s *UserService) existsByEmail(email string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("email = ?", email).
+		Find(&exists).
+		Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByUsername(username string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).Select("count(*) > 0").Where("username = ?", username).Find(&exists).Error;err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByMobileNumber(MobileNumber string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).Select("count(*) > 0 ").Where("mobile_number = ?", MobileNumber).Find(&exists).Error;err != nil{
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return true, nil
+}
+
+func (s *UserService) getDefaultRole() (roleId int, err error) {
+	if err := s.database.Model(&models.Role{}).Select("id").Where("name = ?", constants.DefaultRoleName).Find(&roleId).Error;err != nil {
+		return 0, err
+	}
+	return roleId, nil
+}
