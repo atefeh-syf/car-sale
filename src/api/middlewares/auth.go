40d28d531790e10fb2854d86dde8b69682cd2f7e
atefeh-syf <atefe.s.77218@gmail.com>
2023-11-10 16:23:01 +0330

    Authorization middlewares

--- src/api/middlewares/auth.go
+++ src/api/middlewares/auth.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -33,22 +34,52 @@ func Authentication(cfg *config.Config) gin.HandlerFunc {
 					err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenInvalid}
 				}
 			}
+	}
 
-			if err != nil {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, helper.GenerateBaseResponseWithError(nil, false, -2, err))
-				return				
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, helper.GenerateBaseResponseWithError(nil, false, -2, err))
+		return				
+	}
+
+	c.Set(constants.UserIdKey, claimMap[constants.UserIdKey])
+	c.Set(constants.FirstNameKey, claimMap[constants.FirstNameKey])
+	c.Set(constants.LastNameKey, claimMap[constants.LastNameKey])
+	c.Set(constants.UsernameKey, claimMap[constants.UsernameKey])
+	c.Set(constants.EmailKey, claimMap[constants.EmailKey])
+	c.Set(constants.MobileNumberKey, claimMap[constants.MobileNumberKey])
+	c.Set(constants.RolesKey, claimMap[constants.RolesKey])
+	c.Set(constants.ExpireTimeKey, claimMap[constants.ExpireTimeKey])
+
+	 c.Next()
+}
+
+}
+
+func Authorization(validRoles []string )  gin.HandlerFunc{
+		return func(c *gin.Context) {
+			if len(c.Keys) == 0 {
+				c.AbortWithStatusJSON(http.StatusForbidden, helper.GenerateBaseResponse(nil, false, -300))
+				return
+			}
+			rolesVal := c.Keys[constants.RolesKey]
+			fmt.Println(rolesVal)
+			if rolesVal == nil {
+				c.AbortWithStatusJSON(http.StatusForbidden, helper.GenerateBaseResponse(nil, false, -301))
+				return
 			}
 
-			c.Set(constants.UserIdKey, claimMap[constants.UserIdKey])
-			c.Set(constants.FirstNameKey, claimMap[constants.FirstNameKey])
-			c.Set(constants.LastNameKey, claimMap[constants.LastNameKey])
-			c.Set(constants.UsernameKey, claimMap[constants.UsernameKey])
-			c.Set(constants.EmailKey, claimMap[constants.EmailKey])
-			c.Set(constants.MobileNumberKey, claimMap[constants.MobileNumberKey])
-			c.Set(constants.RolesKey, claimMap[constants.RolesKey])
-			c.Set(constants.ExpireTimeKey, claimMap[constants.ExpireTimeKey])
+			roles := rolesVal.([]interface{})
+			val := map[string]int{}
+			for _, item := range roles {
+				val[item.(string)] = 0
+			}
 
-		 	c.Next()
+			for _, item := range validRoles {
+				if _, ok := val[item]; ok {
+					c.Next()
+					return
+				}
+			}
+			c.AbortWithStatusJSON(http.StatusForbidden, helper.GenerateBaseResponse(nil, false, -301))
 		}
-	}
 }
