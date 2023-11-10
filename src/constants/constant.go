1ecdf71b86a3d7147ae2de52b4f9f4dbb019728f
atefeh-syf <atefe.s.77218@gmail.com>
2023-11-10 16:06:03 +0330

    add Authentication middlewares

--- src/constants/constant.go
+++ src/constants/constant.go
@@ -1,8 +1,20 @@
 package constants
 
 const (
-	AdminRoleName = "admin"
-	DefaultRoleName = "default"
-	DefaultUserName = "admin"
-	RedisOtpDefaultKey = "otp"
+	//User
+	AdminRoleName string = "admin"
+	DefaultRoleName string= "default"
+	DefaultUserName string = "admin"
+	RedisOtpDefaultKey string = "otp"
+
+	// Claims
+	AuthorizationHeaderKey string = "Authorization"
+	UserIdKey              string = "UserId"
+	FirstNameKey           string = "FirstName"
+	LastNameKey            string = "LastName"
+	UsernameKey            string = "Username"
+	EmailKey               string = "Email"
+	MobileNumberKey        string = "MobileNumber"
+	RolesKey               string = "Roles"
+	ExpireTimeKey          string = "Exp"
 )
--- src/pkg/service_errors/error_code.go
+++ src/pkg/service_errors/error_code.go
@@ -4,6 +4,9 @@ const (
 	// Token
 		UnExpectedError = "Expected error"
 		ClaimsNotFound = " Claim not found"
+		TokenRequired = "Token required"
+		TokenExpired = "Token expired"
+		TokenInvalid = "Token invalid"
 	//Otp
 	OtpExists = "Otp Exists"
 	OtpUsed = "Otp Used"
